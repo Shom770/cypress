a1b59ac47286caf380bd90eed5fb058ee530f126
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-09-18 18:16:54 -0400

    multi-line support

--- src/main.kt
+++ src/main.kt
@@ -3,8 +3,10 @@ import java.io.File
 import cypress.Lexer
 
 fun main() {
-    for (line in File("src/test.cy").readLines()) {
-        val lexer = Lexer(line)
-        println(lexer.tokenize())
+    val bufferedReader = File("src/test.cy").bufferedReader()
+    val text = bufferedReader.use { it.readText() }
+    val lexer = Lexer(text)
+    for (token in lexer.tokenize()) {
+        println(token.text(text))
     }
 }
