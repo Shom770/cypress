e05f5ce8eadf67879ac14f055b296595a8615940
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-09-21 21:46:29 -0400

    finished basic interpreters!
    drawback is only doubles

--- src/main.kt
+++ src/main.kt
@@ -6,5 +6,6 @@ fun main() {
     val bufferedReader = File("src/test.cy").bufferedReader()
     val text = bufferedReader.use { it.readText() }
     val tokens = Lexer(text).tokenize()
-    println(Parser(tokens).parse())
+    val nodes = Parser(tokens).parse()
+    println(Interpreter(nodes, text).walk())
 }
