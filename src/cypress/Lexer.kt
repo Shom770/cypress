094489357caef0704bc435efdd0c253aa84ca041
Shom770 <82843611+Shom770@users.noreply.github.com>
2023-02-11 15:11:07 -0500

    redo structure of cypress so far

--- src/cypress/Lexer.kt
+++ src/cypress/Lexer.kt
@@ -3,17 +3,33 @@ package cypress
 
 class Lexer(private val text: String) {
     private val tokens = mutableListOf<Token>()
-    private var currentChar: Char? = null
-    private var position = -1
-        set(value) {
-            field = value
-            currentChar = text.getOrNull(value)
-        }
- 
+    private val textIterator = text.toList().listIterator()
+
+    private val validIdentifiers = ('A'..'z').joinToString("").replace("[\\]^_`", "_")
+    private val validOperators = "+-*/="
+
+    private fun advance(n: Int = 1): IndexedValue<Char> {
+        repeat(n - 1) { textIterator.next() }
+        return IndexedValue(textIterator.nextIndex(), textIterator.next())
+    }
+
     private fun peekAhead(): Char? {
-        return text.getOrNull(position + 1)
+        return text.getOrNull(textIterator.nextIndex())
     }
 
     fun tokenize(): MutableList<Token> {
+        while (textIterator.hasNext()) {
+            val (index, currentChar) = advance()
+            val nextChar = peekAhead()
+            println("$index $currentChar ----- $nextChar")
+//            when (currentChar) {
+//                in validOperators -> parseOperator(index, currentChar)
+//                in validIdentifiers -> parseIdentifier(index, currentChar)
+//            }
+        }
+
+        return tokens
     }
+
+
 }
